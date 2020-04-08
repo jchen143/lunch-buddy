f19651adbb355f49f7dd58ad83dab3049d9a6faf
your name here <jchen143@fordham.edu>
2020-04-08 09:53:34 -0400

    production.rb setup for background image

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.serve_static_assets = true
+  config.serve_static_files = true
   config.assets.compile = true
-  # Code is not reloaded between requests.
+  # Code is not reloaded between requests
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
