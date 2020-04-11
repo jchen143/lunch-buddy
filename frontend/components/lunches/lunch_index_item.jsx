c73452f2e2d424e889c1f01d8d2abfc671045a44
your name here <jchen143@fordham.edu>
2020-04-11 13:45:29 -0400

    edit lunch index item to only include relevant field

--- frontend/components/lunches/lunch_index_item.jsx
+++ frontend/components/lunches/lunch_index_item.jsx
@@ -8,10 +8,10 @@ const LunchIndexItem = (props) => {
         <li className="lunch-li" key={lunch.id}>
             <div className="item-header">Lunch</div>
             <img src={lunch.photoUrl} />
-            <p>{lunch.name}</p>
+            <p className="lunch-name">{lunch.name}</p>
             {/* <p>{lunch.description}</p> */}
-            <p>{restaurant.name}</p>
-            <p>{restaurant.address}</p>
+            <p className="restaurant-name">{restaurant.name}</p>
+            <p className="restaurant-address">{restaurant.address}</p>
             {/* <p>{lunch.size}</p> */}
         </li>
     )
