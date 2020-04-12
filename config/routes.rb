3fbc604796eba73e7c6bd1cc6648ae07ff4555c2
your name here <jchen143@fordham.edu>
2020-04-11 22:09:03 -0400

    added custom LunchBud logo

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     resource :session, only: [:new, :create, :destroy]
     resources :lunches, only: [:index]
     resources :restaurants, only: [:index]
+    resources :reservations, only: [:index, :show, :create, :destroy]
   end
 
 end
