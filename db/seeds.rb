4be3398df213de38ef8c4c0fdfb72cc8ddca6679
your name here <jchen143@fordham.edu>
2020-04-15 20:05:19 -0400

    re-seeded info

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,12 +15,12 @@ require 'open-uri'
 User.create(email: "demo_user@appacademy.io", password: "password");
 
 Restaurant.create([{name: 'App Academy Cafe', address: '90 5th Ave', latitude: 40.7352422, longitude: -73.9960728 }]) 
-Restaurant.create([{name: 'Jos Crabshack', address: '1804 3rd Ave, Apt. 3', latitude: 40.7879917, longitude: -73.9496956 }])#yes, yes
+Restaurant.create([{name: "Jo's Crabshack", address: '1804 3rd Ave, Apt. 3', latitude: 40.7879917, longitude: -73.9496956 }])#yes, yes
 Restaurant.create([{name: 'Lincoln Square Tavern', address: '113 W 60th St', latitude: 40.772826, longitude: -73.9869315 }]) #yes,
 Restaurant.create([{name: 'Central Park Hideaway', address: 'Park Ave',latitude: 40.774768, longitude: -73.971970 }]) 
 Restaurant.create([{name: 'Liberty Island Tacos', address: '1 Main St',latitude: 40.6983762, longitude: -74.0468981 }])
-Restaurant.create([{name: 'World Trade Center Food Court', address: '285 Fulton St',latitude: 40.7115322, longitude: -74.0167379 }])
-Restaurant.create([{name: 'Peter Kim Korean BBQ Extraordinaire', address: '229 W 28 St',latitude: 40.748346, longitude: -73.9961638 }])
+Restaurant.create([{name: 'WTC Food Court', address: '285 Fulton St',latitude: 40.7115322, longitude: -74.0167379 }])
+Restaurant.create([{name: 'Peter Kim BBQ', address: '229 W 28 St',latitude: 40.748346, longitude: -73.9961638 }])
 Restaurant.create([{name: 'Rams Deli', address: '441 East Fordham Rd',latitude: 40.8586227, longitude: -73.8880667 }])
 Restaurant.create([{name: 'Rancho Estrella', address: '616 E 187th St',latitude: 40.8551323, longitude: -73.8892581 }])
 Restaurant.create([{name: 'Staten Island Soup Spot', address: '34 Baker St',latitude: 40.5737968, longitude: -74.1493022 }])
@@ -28,7 +28,7 @@ Restaurant.create([{name: 'Bella', address: '40 Queens Blvd',latitude: 40.744205
 Restaurant.create([{name: 'Risotto e Boccio', address: '43 Queens Blvd',latitude: 40.7433062, longitude: -73.9224616 }])
 Restaurant.create([{name: 'Checkmate in Queens', address: '10 Utopia Pkwy',latitude: 40.7255828, longitude: -73.7941393}])
 Restaurant.create([{name: 'Sweet Chick', address: '10 Utopia Pkwy',latitude: 40.67986932855128, longitude: -73.97645332888183}])
-Restaurant.create([{name: 'Big Danny Brisket and Waffle House', address: '1400 Fulton St',latitude: 40.680317, longitude: -73.943119}])
+Restaurant.create([{name: "Big Danny's", address: '1400 Fulton St',latitude: 40.680317, longitude: -73.943119}])
 
 
 lunch_1 = Lunch.create(name: 'Fullstack Pancakes', description: 'Breakfast for lunch!', image_url: 'placeholder' ,size: 'small',restaurant_id: Restaurant.all[0].id)
