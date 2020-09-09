16d05ccb5fc1ff3a76286d312c92bbe1e41771fc
Jo Chen <jchen143@fordham.edu>
2020-09-09 12:30:21 -0400

    Update marker_manager.js

--- frontend/util/marker_manager.js
+++ frontend/util/marker_manager.js
@@ -32,20 +32,7 @@ export default class MarkerManager {
     createMarkerFromRestaurant(restaurant){
              let myLatLng = new google.maps.LatLng(restaurant.latitude, restaurant.longitude);
 
-            // let meal_info = document.createElement('div'); 
-            
-            // let food_pic_url = restaurant.lunch_url; 
-            // let food_pic = document.createElement('img'); 
-            
-            // food_pic.src = food_pic_url; 
-
-            // meal_info.appendChild(food_pic); 
-           
-        // let infoWindow = new google.maps.InfoWindow({
-        //     content: meal_info
-        // });
-
-        // this.infoWindows[restaurant.id] = infoWindow; 
+       
        
             let marker = new google.maps.Marker({
                 id: restaurant.id,
@@ -60,32 +47,12 @@ export default class MarkerManager {
           
             this.markers[restaurant.id] = marker;   
         
-        
-
-        // let scope_info_windows = this.infoWindows; 
-        
-        // marker.addListener('click', function() {
-        //     infoWindow.open(this.map, marker);
-          
-        //     for(let key in scope_info_windows){
-                
-        //         if(key != marker.id){
-        //             scope_info_windows[key].close(); 
-        //         }
-        //     }
-        // });
-
-        
-
-    
-       // marker.addListener('mouseout', () => this.handleHoverOff(marker));
+   
 
         let lunchItem = document.getElementById(`lunch-${marker.id.toString()}`);
 
         if (lunchItem) {
-            //lunchItem.addEventListener('mouseover', () => marker.setIcon(this.blue_icon));
-            //lunchItem.addEventListener('mouseout', () => marker.setIcon(this.red_icon));
-
+      
             lunchItem.addEventListener('mouseover', () => marker.setAnimation(google.maps.Animation.BOUNCE));
             lunchItem.addEventListener('mouseout', () => marker.setAnimation(null));
         }
